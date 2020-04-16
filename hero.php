351074bc52cf801a7af8aa730ba5cc363258b695
denisgat <denisgatotho@gmail.com>
2020-04-16 16:05:12 -0400

    friendships

--- hero.php
+++ hero.php
@@ -6,52 +6,97 @@ require "header.php";
 
 $id = $_GET["id"];
 
-$sql = "SELECT * FROM heroes";
-$sql2 = "SELECT * FROM relationships WHERE hero1_id = " . $id;
+$sql = "SELECT * FROM heroes WHERE id = " . $id;
 
-//WHERE id = " . $id
-// WHERE hero1_id = " . $id
+$sql2 = "SELECT * 
+FROM ability_hero
+INNER JOIN abilities
+ON ability_hero.ability_id=abilities.id
+WHERE ability_hero.hero_id = " . $id;
+
+$sql3 = "SELECT * 
+FROM relationships  
+INNER JOIN heroes 
+ON relationships.hero2_id=heroes.id
+WHERE relationships.hero1_id =" . $id;
+
+
+
 
 $result = $conn->query($sql);
 $result2 = $conn->query($sql2);
+$result3 = $conn->query($sql3);
+
+
 
 $bio ='';
 $freinds='';
+$ability='';
+$navbar = 
+    '<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">HeroBook</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Info</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Setting</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>';
+
+
+
 
 if ($result->num_rows > 0) {
     //loops through all heros
     while ($herorow = $result->fetch_assoc()) {
-        //enter if when the looping hero matches the hero on the page
-        if($herorow['id'] == $id){
-            $bio = 
-            "<div class ='row'>
-                <div class = 'col-12'>
-                    <a class='btn btn-primary mt-3' href='index.php'> Back </a>
-                    <br></br>
-                    <img src='$herorow[image_url]' class='card-image' alt='profile pic' style='height: 12rem; width: 12rem;'>
-                    <br></br>
-                    <h3> $herorow[name] </h3>
-                    <p> $herorow[biography] </p>
-                    <br></br>
-                </div>
+        $bio = 
+            "<div class = 'col-5 ml-1'>
+                <a class='btn btn-primary mt-3' href='index.php'> Back </a>
+                <br></br>
+                <img src='$herorow[image_url]' class='card-image ml-5' alt='profile pic' style='height: 14rem; width: 14rem;'>
+                <br></br>
+                <h3> $herorow[name] </h3>
+                <p> $herorow[biography] </p>
+                <br></br>
             </div>";
-        }
-        //loops through the relationship table where the hero1_id matches the hero on the page
-        while($relationrow = $result2->fetch_assoc()){
-            //enters if when the looping hero matches the hero2_id in the relationtable
-            // if($herorow['id'] == $relationrow['hero2_id'] && hero1_id = $id){
-                $friends .= 
-                    "<div class='row'>
-                        <div class='col-12'>
-                            <h3>Friend</h3>
-                            <ul>
-                                <li> Friend ID: $relationrow[hero2_id] </li>
-                                <li> Friend Name: $herorow[name] </li>
-                            </ul>
-                        </div.
-                    </div>";
-            // }
-        }
+    }
+
+    while($abilityrow = $result2->fetch_assoc()){
+        $ability = 
+            "<div class = 'col-5 ml-1'>
+                <h3> SuperPower</h3>
+                <br></br>
+                <h3> $abilityrow[ability] </h3>
+            </div>";
+    }
+
+    while($relationrow = $result3->fetch_assoc()){
+        $hero = "hero.php?id=" . $relationrow["id"];
+        $friends .= 
+            '<div class="col-3 text-dark">
+                <div class="card m-2 bg-light" style="height: 27rem; width: 15rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">' . $relationrow["name"] . '</h5>
+                        <img src=' . $relationrow["image_url"] .' class="card-image" alt="profile pic" style="height: 12rem; width: 12rem;">
+                        <p class="card-text my-3">' . $relationrow["about_me"] . '</p>
+                        <a href=' . $hero . ' class="btn btn-primary">About me</a>
+                    </div>
+                 </div>
+            </div>';
     }
 } 
 
@@ -59,16 +104,23 @@ else {
     echo "0 results";
 }
 
+$render = 
+    "<div>
+        $navbar
+        <div class='container bg-dark text-white' style='height: 100%'>
+            <div class='row'>
+            $bio
+            $ability
+            </div>
+            <h3 class='text-center'> Friends </h3>
+            <br></br>
+            <div class='row'>
+            $friends
+            </div>
+        </div>
+    </div>";
 
-
-echo "<div class='container bg-dark text-white' style='height: 100%'>";
-echo $bio;
-echo $friends;
-echo "</div>";
-
-$sql = "SELECT * FROM relationships WHERE hero1_id = $id OR hero2_id = $id AND type_id = 1";
-$result = $conn->query($sql);
-// var_dump($result) ;
+echo $render;
 
 $conn->close();
 
